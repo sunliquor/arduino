3fc760fa49b1163c4405dc0cd45b65854b8d151f
sunliquor <62785227+sunliquor@users.noreply.github.com>
2022-05-05 19:44:58 +0800

    Add files via upload

--- SerialPassthrough.ino
+++ SerialPassthrough.ino
@@ -1,16 +1,30 @@
+#include<WiFi.h>
 
+const char ssid[]="robin"; //修改為你家的WiFi網路名稱
+const char pwd[]="mochi0607"; //修改為你家的WiFi密碼
 
 void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA); //設置WiFi模式
+  WiFi.begin(ssid,pwd); 
+
+  Serial.print("WiFi connecting");
+
+  //當WiFi連線時會回傳WL_CONNECTED，因此跳出迴圈時代表已成功連線
+  while(WiFi.status()!=WL_CONNECTED){
+    Serial.print(".");
+    delay(500);   
+  }
+
+  Serial.println("");
+  Serial.print("IP位址:");
+  Serial.println(WiFi.localIP()); //讀取IP位址
+  Serial.print("WiFi RSSI:");
+  Serial.println(WiFi.RSSI()); //讀取WiFi強度
+  
+
 }
 
 void loop() {
-  if (Serial.available()) {      // If anything comes in Serial (USB),
-    Serial1.write(Serial.read());   // read it and send it out Serial1 (pins 0 & 1)
-  }
-
-  if (Serial1.available()) {     // If anything comes in Serial1 (pins 0 & 1)
-    Serial.write(Serial1.read());   // read it and send it out Serial (USB)
-  }
 }
